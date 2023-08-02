4a6704a1b25d7bb5fa80f5e436a4457ba3f4aa07
Mohammed Wajid <wajid17023@gmail.com>
2023-08-03 01:49:02 +0530

    added services section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import GreenBtn from "./Components/GreenBtn";
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
+import Services from "./Components/Services";
 
 function App() {
   return (
     <div >
      <Navbar/>
      <GreenBtn/>
+     <Hero/>
+     <Services/>
     </div>
   );
 }
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import hero from "../Assets/hero.jpg";
+import GreenBtn from "./GreenBtn";
 
 const Hero = () => {
   return (
-    <div></div>
-  )
-}
+    <div
+      name="home"
+      className="flex flex-col justify-between w-full h-screen bg-gray-900 text-white text-center md:text-left"
+    >
+      <div className="grid md:grid-cols-2 max-w-screen-xl m-auto px-3">
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+          <p className="text-2xl text-gray-500">
+            Cheapest Hoisting on Planet Earth
+          </p>
+          <h1 className="pt-1 pb-6 text-5xl md:text-7xl font-bold">
+            <span className="text-lightColor">CNN</span> Web Services
+          </h1>
+          <p className="text-base font-light text-gray-500">
+            Explore cushions he with fowl was nodded, merely suddenly thee bore
+            nevermore. My bust sitting the ease of the will.
+          </p>
+          <GreenBtn
+            title="sign up"
+            className="capitalize py-3 px-6 sm:w-8/12 my-8"
+          />
+        </div>
 
-export default Hero
+        <div className="flex items-center justify-center">
+          <img src={hero} alt="hero" className="w-3/4 rounded-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
